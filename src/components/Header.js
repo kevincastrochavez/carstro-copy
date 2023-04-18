d0427363a033f2d11d822dbc93a0609ebb9b5d37
Kevin Castro <kevin.castro@familysearch.org>
2023-04-17 21:29:14 -0600

    fixed build and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,9 +17,9 @@ function Header() {
       // window.location.href === 'http://localhost:3000/marketing'
 
       // Production
-      window.location.href === 'https://carstro-15495.web.app/' ||
-      window.location.href === 'https://carstro-15495.web.app/aboutUs' ||
-      window.location.href === 'https://carstro-15495.web.app/marketing'
+      window.location.href === 'https://carstro-copy.web.app/' ||
+      window.location.href === 'https://carstro-copy.web.app/aboutUs' ||
+      window.location.href === 'https://carstro-copy.web.app/marketing'
     ) {
       setHeaderWhite(false);
     } else {
